6eede73f74ed19faa53c8af38e22b364342208b7
Vinnie Marone <vinnie.marone35@gmail.com>
2022-08-26 13:36:16 -0400

    Add a comment to link to php documentation

--- src/Time/TimeUtil.php
+++ src/Time/TimeUtil.php
@@ -9,6 +9,12 @@ use DateTime;
 
 class TimeUtil
 {
+	/**
+	 * @see https://www.php.net/manual/en/class.dateinterval.php
+	 * @param $start_date - Ex: 2022-04-19 12:55:00
+	 * @param $end_date - Ex 2022-09-20 14:16:00
+	 * @return DateInterval
+	 */
 	public static function getAge(string $start_date, string $end_date) : DateInterval
 	{
 		return (new DateTime($start_date))->diff(new DateTime($end_date));
